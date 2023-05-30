5b15e4ab3711cde3526af35c07aa91d063026394
Alexander <aleksievisa@gmail.com>
2023-05-30 15:17:21 +0300

    feat: join banner

--- client/app/components/JoinFiver/JoinFiver.tsx
+++ client/app/components/JoinFiver/JoinFiver.tsx
@@ -1,7 +1,25 @@
 import styles from './JoinFiver.module.scss'
+import ImageSignUp from '../../assets/bg-signup.webp'
+import Image from 'next/image';
 
 export const JoinFiver = () => {
   return (
-    <div>JoinFiver</div>
-  )
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <h4 className={styles.text}>
+          Suddenly it&apos;s all so <i>doable.</i>
+        </h4>
+        <button
+          className={styles.btn}
+          type="button"
+        >
+          Join Fiverr
+        </button>
+      </div>
+      <div className=" w-full h-96">
+        <Image src={ImageSignUp} alt="signup" className="rounded-sm" />
+      </div>
+    </div>
+  );
+
 }
