d5afbb4c7dc3ce0d1beb4af66bfdba3a25439b1f
Alexander <aleksievisa@gmail.com>
2023-05-30 13:11:38 +0300

    feat: add services

--- client/app/components/Services/Services.tsx
+++ client/app/components/Services/Services.tsx
@@ -1,7 +1,21 @@
+import { categories } from '@/app/utils/categories'
 import styles from './Services.module.scss'
+import Image from 'next/image'
+
+
 
 export const Services = () => {
   return (
-    <div>Services</div>
+    <div className={styles.wrapper}>
+      <h3 className={styles.title}>You need it, we`ve got it</h3>
+      <ul className={styles.container}>
+        {categories.map((item) => (
+          <li className={styles.item}>
+            <Image src={item.logo} alt='' />
+            <span className={styles.name}>{item.name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
---
+++ client/app/utils/categories.ts
@@ -0,0 +1,23 @@
+import ImageService1 from '../assets/service-1.svg'
+import ImageService2 from '../assets/service-2.svg'
+import ImageService3 from '../assets/service-3.svg'
+import ImageService4 from '../assets/service-4.svg'
+import ImageService5 from '../assets/service-5.svg'
+import ImageService6 from '../assets/service-6.svg'
+import ImageService7 from '../assets/service-7.svg'
+import ImageService8 from '../assets/service-8.svg'
+import ImageService9 from '../assets/service-9.svg'
+import ImageService10 from '../assets/service-10.svg'
+
+export const categories = [
+  { name: "Graphic Design", logo: ImageService1 },
+  { name: "Digital Marketing", logo: ImageService2 },
+  { name: "Writing & Translation", logo: ImageService3 },
+  { name: "Video & Animation", logo: ImageService4 },
+  { name: "Music & Audio", logo: ImageService5 },
+  { name: "Programming & Tech", logo: ImageService6 },
+  { name: "Business", logo: ImageService7 },
+  { name: "Lifestyle", logo: ImageService8 },
+  { name: "Data", logo: ImageService9 },
+  { name: "Photography", logo: ImageService10 },
+];
